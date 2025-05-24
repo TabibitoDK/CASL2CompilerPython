defa834a3a8a3062a5fffbf7fef1a5b3a0b6b8e1
JeorgeC <jcteoh88@gmail.com>
2025-05-25 01:01:00 +0900

    Updating function

--- main.py
+++ main.py
@@ -1,12 +1,96 @@
 
-# 'h' stands for signed 16-bit integer
-MEM = [None] * 65536
-print(MEM)
-print(MEM[0])  # Accessing elements
+
+class Value:
+   def __init__(self, val):
+      self.value = [0] * 16
+      
+   def get(self):
+      return self.value
 
 
 LABELS = []
 INSTRUCTIONS = []
+
+
+
+# Execution Logic
+
+
+GR = {
+   "GR0" : 0,
+   "GR1" : None,
+   "GR2" : None,
+   "GR3" : None,
+   "GR4" : None,
+   "GR5" : None,
+   "GR6" : None,
+   "GR7" : None,
+}
+
+FR = {
+   "OF": 0,
+   "SF": 0,
+   "ZF": 0,
+}
+
+MEM = [None] * 65536
+
+class Execution:
+   def LD(r1, r2, x=0):
+      if isGR(r2):
+         GR[r1] = GR[r2]
+      else:
+         GR[r1] = MEM[r2+x]
+
+   def ST(r1, adr, x=0):
+      MEM[adr+x] = GR[r1]
+   
+   def LAD(r1, r2, x=0):
+      GR[r1] = r2+x
+   
+   def AND(r1, r2, x=0):
+      if isGR(r2):
+         GR[r1] = GR[r1] & GR[r2]
+      else:
+         GR[r1] = GR[r1] & MEM[r2+x]
+
+   def OR(r1, r2, x=0):
+      if isGR(r2):
+         GR[r1] = GR[r1] | GR[r2]
+      else:
+         GR[r1] = GR[r1] | MEM[r2+x]
+
+   def XOR(r1, r2, x=0):
+      if isGR(r2):
+         GR[r1] = GR[r1] ^ GR[r2]
+      else:
+         GR[r1] = GR[r1] ^ MEM[r2+x] 
+
+   def CPA(r1, r2, x=0):
+      if isGR(r2):
+         res = GR[r1] ^ GR[r2]
+      else:
+         res = GR[r1] ^ MEM[r2+x]
+      flagRegist(res)       
+
+
+
+def isGR(op):
+   for i in range(1,8):
+      if op == (f"GR{i}"):
+         return True
+   return False
+
+def flagRegist(data):
+   if data == 0:
+      FR["ZF"] = 1
+   else:
+      FR["ZF"] = 0
+
+   if data > 0:
+      FR["SF"] = 0
+
+# Compiler logic 
 
 
 class Label:
